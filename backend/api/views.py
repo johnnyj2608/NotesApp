bdb6568bc40d0217e5607c17bb9af0de660f15f7
Johnny Jiang <johnnyj2608@gmail.com>
2023-11-19 16:49:51 -0500

    Update note functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -55,3 +55,14 @@ def getNote(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
-import { Link } from "react-router-dom";
 
 const NotePage = () => {
   let { id } = useParams();
   let [note, setNote] = useState(null);
+  let navigate = useNavigate();
 
   useEffect(() => {
     const getNote = async () => {
@@ -16,16 +16,41 @@ const NotePage = () => {
     getNote();
   }, [id]);
 
+  let CSRF = document.cookie.slice(10);
+  let updateNote = async () => {
+    fetch("/api/notes/" + id + "/update/", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": CSRF,
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let handleSubmit = () => {
+    updateNote();
+    navigate("/");
+  };
+
+  let handleChange = (value) => {
+    setNote((note) => ({ ...note, body: value }));
+    console.log("Handle Change:", note);
+  };
+
   return (
     <div className="note">
       <div className="note-header">
         <h3>
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
+          <ArrowLeft onClick={handleSubmit} />
         </h3>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 };
