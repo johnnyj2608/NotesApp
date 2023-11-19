70be7a9e6c3d060d74c9f863275f362bc15279d0
Johnny Jiang <johnnyj2608@gmail.com>
2023-11-19 18:05:09 -0500

    Create note functionality

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -3,22 +3,23 @@ import { useParams, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = () => {
-  let { id } = useParams();
+  let { id: noteId } = useParams();
   let [note, setNote] = useState(null);
   let navigate = useNavigate();
   let CSRF = document.cookie.slice(10);
 
   useEffect(() => {
     const getNote = async () => {
-      let response = await fetch(`/api/notes/${id}`);
+      if (noteId === "new") return;
+      let response = await fetch(`/api/notes/${noteId}`);
       let data = await response.json();
       setNote(data);
     };
     getNote();
-  }, [id]);
+  }, [noteId]);
 
   let updateNote = async () => {
-    fetch("/api/notes/" + id + "/update/", {
+    fetch("/api/notes/" + noteId + "/update/", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -28,8 +29,19 @@ const NotePage = () => {
     });
   };
 
+  let createNote = async () => {
+    fetch("/api/notes/create/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": CSRF,
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
   let deleteNote = async () => {
-    fetch("/api/notes/" + id + "/delete/", {
+    fetch("/api/notes/" + noteId + "/delete/", {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -40,13 +52,20 @@ const NotePage = () => {
   };
 
   let handleSubmit = () => {
-    updateNote();
+    //console.log("NOTE:", note);
+    if (noteId !== "new" && note.body === "") {
+      deleteNote();
+    } else if (noteId !== "new") {
+      updateNote();
+    } else if (noteId === "new" && note.body !== null) {
+      createNote();
+    }
     navigate("/");
   };
 
   let handleChange = (value) => {
     setNote((note) => ({ ...note, body: value }));
-    console.log("Handle Change:", note);
+    //console.log("Handle Change:", note);
   };
 
   return (
@@ -55,7 +74,11 @@ const NotePage = () => {
         <h3>
           <ArrowLeft onClick={handleSubmit} />
         </h3>
-        <button onClick={deleteNote}>Delete</button>
+        {noteId !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
       </div>
       <textarea
         onChange={(e) => {
